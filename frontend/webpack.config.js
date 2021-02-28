58f3e8f84f4b39dcc8ba32f65b2efb208f9a61e6
togo-mentor <uint2435@gmail.com>
2021-03-01 07:17:09 +0900

    feat: create webpack.config.js

---
+++ frontend/webpack.config.js
@@ -0,0 +1,31 @@
+const { VueLoaderPlugin } = require("vue-loader");
+const path = require('path');
+module.exports = {
+  mode: "development",
+  entry: './src/main.js',
+  output: {
+    path: `${__dirname}/../public/javascripts`,
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        exclude: /node_modules/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    watchContentBase: true,
+    publicPath: '/public/javascripts',
+    openPage: 'index.html'
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
+    }
+  },
+  plugins: [new VueLoaderPlugin()]
+} 
