beec1738dd40308a82527787d9cf383053b6ead1
togo-mentor <uint2435@gmail.com>
2021-03-08 08:03:50 +0900

    feat: apply to header display flex

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Header = styled.header`
+    display: flex;
+`;
+
 class App extends React.Component {
   render () {
     return (
       <div className="container">
-        <header>
-
-        </header>
-
-        <h1>Hello! React</h1>
+        <Header>
+          <h1>俺の推薦図書</h1>
+          <div className="header-right">
+            <a href="" className="user-links">新規登録</a>
+            <a href="" className="user-links">ログイン</a>
+          </div>
+        </Header>
       </div>
     )
   } 
