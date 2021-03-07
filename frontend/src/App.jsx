ec86f557a5f46f3d921bd3de9e1f35ef181a5c2d
togo-mentor <uint2435@gmail.com>
2021-03-08 07:24:24 +0900

    fix: import modules short

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+class App extends React.Component {
+  render () {
+    return (
+      <header>
+
+      </header>
+      <div className="container">
+        Hello! React
+      </div>
+    )
+  } 
+}
