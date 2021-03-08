61b60d7e80ddd3c977842e33debb8f1a0880e1eb
togo-mentor <uint2435@gmail.com>
2021-03-09 07:42:03 +0900

    markup: displayed modal on top page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,52 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 
+function UserModal(props) {
+  if (props.show) {
+  return (
+      <ModalOverlay>
+        <ModalContent>
+          <p>これがモーダルウィンドウです。</p>
+          <p><button>close</button></p>
+        </ModalContent>
+      </ModalOverlay>
+   )
+  } else {
+    return null;
+  }
+}
+
+
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      showModal: false,
+    }
+    this.openModal = this.openModal.bind(this)
+  }
+
+
+  openModal() {
+    this.setState ({
+      showModal: true
+    })
+  }
+
   render () {
     return (
       <div className="container">
         <Header>
           <HeaderTitle>俺の推薦図書</HeaderTitle>
           <HeaderRight>
-            <HeaderLink>新規登録</HeaderLink>
-            <HeaderLink>ログイン</HeaderLink>
+            <HeaderLink onClick={this.openModal}>
+              新規登録
+              <UserModal show={this.state.showModal}/>
+            </HeaderLink>
+            <HeaderLink onClick={this.openModal}>
+              ログイン
+              <UserModal show={this.state.showModal}/>
+            </HeaderLink>
           </HeaderRight>
         </Header>
       </div>
@@ -51,6 +88,28 @@ const HeaderLink = styled.a`
     background-color: #fff;
     color: lightgray; 
   }
+`
+
+const ModalOverlay = styled.div `
+  /*　画面全体を覆う設定　*/
+  position:fixed;
+  top:0;
+  left:0;
+  width:100%;
+  height:100%;
+  background-color:rgba(0,0,0,0.5);
+
+  /*　画面の中央に要素を表示させる設定　*/
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const ModalContent = styled.div `
+  z-index:2;
+  width:50%;
+  padding: 1em;
+  background:#fff;
 `
 
 
