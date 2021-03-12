db4e793f14cd96058875a0028c280fcf9ab50113
togo-mentor <uint2435@gmail.com>
2021-03-13 08:13:41 +0900

    feat: model validation restored

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  has_one_attached :avatar
+  
+  with_options presence: true, { message: 'は必須です'} do 
+    validates :nickname
+    validates :image
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]+\z/.freeze #半角で英小文字・大文字・数字全て含む
+  validates :password, format { with: PASSWORD_REGEX, message: 'は英語大文字・小文字・数字を全て含む形式にしてください'}
+  validates :password, length: { in: 6..20, message: 'は6文字以上20文字以下で設定してください' }
 end
