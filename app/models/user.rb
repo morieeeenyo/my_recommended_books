680a3d5eb050efc1de2d5e22ca65b3219f4c3d3d
togo-mentor <uint2435@gmail.com>
2021-03-13 07:43:11 +0900

    feat: validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :avatar
+  
+  with_options presence: true, { message: 'は必須です'} do 
+    validates :nickname
+    validates :image
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]+\z/.freeze #半角で英小文字・大文字・数字全て含む
+  validates :password, format { with: PASSWORD_REGEX, message: 'は英語大文字・小文字・数字を全て含む形式にしてください'}
+  validates :password, length: { in: 6..20, message: 'は6文字以上20文字以下で設定してください' }
 end
