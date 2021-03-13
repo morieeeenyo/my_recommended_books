bcb08f03c44d2f3de82ee975c9b6ed3dcb5c654e
togo-mentor <uint2435@gmail.com>
2021-03-13 09:18:32 +0900

    feat: create api v1 registarions controller

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,9 +1,8 @@
 module Api
-  module Auth
+  module V1
     class RegistrationsController < DeviseTokenAuth::RegistrationsController
 
       private
-      # :company( 企業名 )を追加できるようにpravateメソッドに修正を加える
       def sign_up_params
         params.permit(:nickname, :email, :avatar, :password, :password_confirmation)
       end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
-  before_action :configure_permitted_parameters, if: :devise_controller? 
-
-  private
-
-  def configure_permitted_parameters
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-  end 
 end
