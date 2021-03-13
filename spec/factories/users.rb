1c1c014d38b093365c750995888bbf4ecd3fab4d
togo-mentor <uint2435@gmail.com>
2021-03-13 17:16:56 +0900

    test: generate factory bot user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,11 @@
 FactoryBot.define do
   factory :user do
-    nickname {}
-    sequence(:email) { |n| "TEST#{n}@example.com"}
+    nickname {Faker::Name.name}
+    email {Faker::Internet.free_email}
+    password {'Pass1234'}
+    password_confirmation {'Pass1234'}
+    after(:build) do |user|
+      user.avatar.attach(io: File.open('spec/fixtures/test_image.jpg'), filename: 'test_image.jpg', content_type: 'image/jpg')
+    end
   end
 end
