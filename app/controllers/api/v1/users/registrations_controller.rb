59b9b75745e5c077dceec158a284848f8ee36fa8
togo-mentor <uint2435@gmail.com>
2021-03-13 16:49:48 +0900

    fix: change directly and routing of registration to specify that it is used for users

---
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -0,0 +1,26 @@
+module Api
+  module V1
+    module Users
+      class RegistrationsController < DeviseTokenAuth::RegistrationsController
+        respond_to :json
+
+        def create 
+          @user = User.new(sign_up_params)
+          if @user.valid?
+            @user.save 
+            render json: {user: @user}
+          else
+            render json: {errors: @user.errors.full_messages}
+          end
+        end
+
+        private
+
+        def sign_up_params
+          params.require(:registration).permit(:nickname, :email, :avatar, :password, :password_confirmation)
+        end
+
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do 
       mount_devise_token_auth_for 'User', at: 'users' , controllers: {
-        registrations: 'api/v1/registrations'
+        registrations: 'api/v1/users/registrations'
       }
     end
   end
