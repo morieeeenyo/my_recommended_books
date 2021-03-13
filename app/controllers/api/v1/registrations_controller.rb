c0ea42c248419e592fa3fe951e37db9fb2a72d68
togo-mentor <uint2435@gmail.com>
2021-03-13 09:30:52 +0900

    refactor: deleted update params because not neccesary now

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -3,12 +3,9 @@ module Api
     class RegistrationsController < DeviseTokenAuth::RegistrationsController
 
       private
+      
       def sign_up_params
         params.permit(:nickname, :email, :avatar, :password, :password_confirmation)
-      end
-
-      def account_update_params
-        params.permit(:name, :email, :company)
       end
 
     end
