f85bd0b1e60c478df3a75bd692366ce70aa7fc47
togo-mentor <uint2435@gmail.com>
2021-03-22 06:52:31 +0900

    doc: commented

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -1,21 +1,21 @@
 class Api::V1::Users::RegistrationsController < DeviseTokenAuth::RegistrationsController
   skip_before_action :verify_authenticity_token, only: [:create] # APIではCSRFチェックをしない
-  after_action :set_csrf_token_header
+  after_action :set_csrf_token_header #csrf-tokenの更新
   
   respond_to :json
 
   def create 
     @user = User.new(sign_up_params)
     if @user.valid?
-      if params[:user][:avatar][:data] != "" && params[:user][:avatar][:filename] != "" #画像データ自体は送られてくるので中身の判定をする
+      if params[:user][:avatar][:data] != "" && params[:user][:avatar][:filename] != "" #画像データ自体は送られてくるので中身が空かどうか判定をする
         blob = ActiveStorage::Blob.create_after_upload!(
-          io: StringIO.new(decode(params[:user][:avatar][:data]) + "\n"),
-          filename: params[:user][:avatar][:filename]
+          io: StringIO.new(decode(params[:user][:avatar][:data]) + "\n"), #UserModal.jsxでfilereaderを使って取得した文字列を復号する
+          filename: params[:user][:avatar][:filename] #filenameはUserModal.jsxで取得
         )
-        @user.avatar.attach(blob)
+        @user.avatar.attach(blob) #先に作っておいた画像とuserを紐付ける
       end
       @user.save 
-      update_auth_header
+      update_auth_header #access-token, clientの発行
       render json: { user: @user }
     else
       render status: 422, json: { errors: @user.errors.full_messages } #手動でステータス入れないと200になるぽい
@@ -42,14 +42,8 @@ class Api::V1::Users::RegistrationsController < DeviseTokenAuth::RegistrationsCo
     @token.client = nil unless @used_auth_by_token
     if @used_auth_by_token && !DeviseTokenAuth.change_headers_on_each_request
       auth_header = @user.build_auth_header(@token.token, @token.client)
-      response.headers.merge!(auth_header)  
+      response.headers.merge!(auth_header)  #access-tokenとclientをresponse.headersにマージ
     else
-      unless @user.reload.valid?
-        @user = @user.class.find(@user.to_param) 
-        unless @user.valid?
-          raise DeviseTokenAuth::Errors::InvalidModel, "Cannot set auth token in invalid model. Errors: #{@resource.errors.full_messages}"
-        end
-      end
       refresh_headers
     end
   end
--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -1,17 +1,17 @@
 class Api::V1::Users::SessionsController < DeviseTokenAuth::SessionsController
-  skip_before_action :verify_authenticity_token, only: [:create] # APIではCSRFチェックをしない
-  before_action :set_user_by_token, only: [:destroy]
-  after_action :set_csrf_token_header
-  after_action :reset_session, only: [:destroy]
+  skip_before_action :verify_authenticity_token, only: [:create] # ログイン時はCSRFチェックをしない
+  after_action :set_csrf_token_header #csrf-tokenの更新
+  after_action :reset_session, only: [:destroy] #session情報を削除
   respond_to :json
 
   def create
     @user = User.find_for_database_authentication(email: params[:user][:email])
+    # 厳密にemailとpassword同時に判定しているわけではない
     return invalid_email unless @user 
 
     if @user.valid_password?(params[:user][:password])
       sign_in :user, @user
-      update_auth_header
+      update_auth_header #responseにuid, client, access-tokenを含ませて今後の処理で使えるようにする
       render json: {user: @user}
     else
       invalid_password
@@ -22,7 +22,7 @@ class Api::V1::Users::SessionsController < DeviseTokenAuth::SessionsController
     @user = User.find_for_database_authentication(uid: request.headers['uid'])
     @token = request.headers['access-token']
     @client = request.headers['client']
-    if @user && @client && @token
+    if @user && @client && @token #uid, client, access-tokenの3つが揃ったときだけログアウトできる。これはdevise-auth-tokenのデフォルトの処理でもある
       @token.clear 
       @client = nil
       render_destroy_success
@@ -53,14 +53,8 @@ class Api::V1::Users::SessionsController < DeviseTokenAuth::SessionsController
     @token.client = nil unless @used_auth_by_token
     if @used_auth_by_token && !DeviseTokenAuth.change_headers_on_each_request
       auth_header = @user.build_auth_header(@token.token, @token.client)
-      response.headers.merge!(auth_header)  
+      response.headers.merge!(auth_header)  #ここでheaderにaccess-token, clientをマージ
     else
-      unless @user.reload.valid?
-        @user = @user.class.find(@user.to_param) 
-        unless @user.valid?
-          raise DeviseTokenAuth::Errors::InvalidModel, "Cannot set auth token in invalid model. Errors: #{@resource.errors.full_messages}"
-        end
-      end
       refresh_headers
     end
   end
