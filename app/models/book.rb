79929d17d9bdd140399435d2be176af9401c031a
togo-mentor <uint2435@gmail.com>
2021-03-26 07:56:10 +0900

    feat: validation on books model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,14 @@
 class Book < ApplicationRecord
+  has_one_attached :image
+
+  with_options presence: true do 
+    validates :title
+    validates :recommends
+    validates :author
+    validates :publisher
+    validates :genre_id
+    validates :price
+    validates :description
+    validates :amazon_link
+  end
 end
