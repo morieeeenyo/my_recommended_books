da6617a824c50221b35ba9204f15fe42641ad872
togo-mentor <uint2435@gmail.com>
2021-03-27 08:46:43 +0900

    fix: change rakuten id into environment
    API keyは変更済み

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,4 +1,4 @@
 RakutenWebService.configure do |c|
-  c.application_id = '1002554735665735276'
-  c.affiliate_id = 'a3e619234bdb25ee0f918ef2d3680d1ad464e29c'
+  c.application_id = ENV['RAKUTEN_APP_ID']
+  c.affiliate_id = ENV['RAKUTEN_APP_AFFILIATE_ID']
 end
