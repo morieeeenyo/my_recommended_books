02e10ff9c2212477391d469abc6b81ddd70d477f
togo-mentor <uint2435@gmail.com>
2021-03-27 07:29:16 +0900

    doc: commented search params

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -2,5 +2,16 @@ class Api::V1::BooksController < ApplicationController
   def search
     @books = RakutenWebService::Books::Book.search(booksGenreId: "001005")
      render json: { status: 'success', data: @books }  
+    #  検索で使うデータ(候補)
+    #   authorKana→author
+    #   title
+    #   publisherName
+    #   mediumImageUrl
+    #   smallImageUrl
+    #   itemPrice
+    #   affiliateUrl
+    #   itemUrl
+    #   genreId
+    #   salesDate
   end
 end
