1fe384821abdab9ee7d4dd601a297b59c2f06c50
togo-mentor <uint2435@gmail.com>
2021-03-27 15:28:09 +0900

    feat: generate user book model to asoociate many user and many books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_25_224848) do
+ActiveRecord::Schema.define(version: 2021_03_27_062451) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_03_25_224848) do
   end
 
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "isbn", null: false
     t.string "title", null: false
     t.string "author", null: false
     t.string "author_kana", null: false
@@ -46,6 +47,15 @@ ActiveRecord::Schema.define(version: 2021_03_25_224848) do
     t.string "recommends", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "book_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_user_books_on_book_id"
+    t.index ["user_id"], name: "index_user_books_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -72,4 +82,6 @@ ActiveRecord::Schema.define(version: 2021_03_25_224848) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "user_books", "books"
+  add_foreign_key "user_books", "users"
 end
