8c1ba5a0a5d60bd916694c59ae8437c0aded57d3
togo-mentor <uint2435@gmail.com>
2021-03-28 10:54:26 +0900

    feat: separate form title search and manual post

--- frontend/src/components/books/NewBookModal.jsx
+++ frontend/src/components/books/NewBookModal.jsx
@@ -9,6 +9,73 @@ import {FormBlock} from "../common/UserModal.jsx"
 import {ModalOverlay} from "../common/UserModal.jsx"
 import {ModalContent} from "../common/UserModal.jsx"
 
+function manualBookForm() {
+    return(
+      <form>
+        <BooksFormBlock>
+          <label htmlFor="title">タイトル</label>
+          <input type="text" name="title" id="nickname" onChange={this.searchBook}/>  
+        </BooksFormBlock>
+        <BooksFormBlock>
+          <label htmlFor="recommends">こんな人におすすめ！</label>
+          <input type="text" name="recommends" id="nickname" />  
+        </BooksFormBlock>
+        <BooksFormBlock>
+          <label htmlFor="description">内容要約</label>
+          <textarea type="text" name="description" id="nickname"></textarea> 
+        </BooksFormBlock>
+        <BooksFormBlock>
+          <label htmlFor="author">著者</label>
+          <input type="text" name="author" id="nickname" />  
+        </BooksFormBlock>
+        <BooksFormBlock>
+          <label htmlFor="author_kana">著者(カナ)</label>
+          <input type="text" name="author_kana" id="nickname" />  
+        </BooksFormBlock>
+        <BooksFormBlock>
+          <label htmlFor="publisher_name">出版社</label>
+          <input type="text" name="publisher_name" id="nickname" />  
+        </BooksFormBlock>
+        <BooksFormBlock>
+          <label htmlFor="genre_id">ジャンル</label>
+          <input type="text" name="genre_id" id="nickname" />  
+        </BooksFormBlock>
+        <BooksFormBlock>
+          <label htmlFor="sales_date">発売日</label>
+          <input type="text" name="sales_date" id="nickname" />  
+        </BooksFormBlock>
+        <BooksFormBlock>
+          <label htmlFor="item_price">価格</label>
+          <input type="text" name="item_price" id="nickname" />  
+        </BooksFormBlock>
+        <BooksFormBlock>
+          <label htmlFor="image_url">画像</label>
+          <input type="hidden" name="image_url" id="nickname" />  
+        </BooksFormBlock>
+        <BooksFormBlock>
+          <input type="hidden" name="isbn" id="nickname" />  
+        </BooksFormBlock>
+        <BooksFormBlock>
+          <input type="submit" value="推薦図書に追加" id="submit-btn"/>
+        </BooksFormBlock>
+      </form>
+    )
+  }
+
+function SearchBookForm(props) {
+  return(
+    <form>
+      <BooksFormBlock>
+        <label htmlFor="title">タイトル</label>
+        <input type="text" name="title" id="nickname" onChange={props.search}/>  
+      </BooksFormBlock>
+      <BooksFormBlock>
+        <input type="submit" value="推薦図書に追加" id="submit-btn"/>
+      </BooksFormBlock>
+    </form>
+  )
+}
+
 class NewBookModal extends React.Component {
   constructor(props){
     super(props);
@@ -67,55 +134,7 @@ class NewBookModal extends React.Component {
         <p>推薦図書を投稿する</p>
         <button onClick={this.closeBookModal}>x</button>
           <NewBooksWrapper>
-            <form>
-              {/* name属性とかは変更していない状態 */}
-              <BooksFormBlock>
-                <label htmlFor="title">タイトル</label>
-                <input type="text" name="title" id="nickname" onChange={this.searchBook}/>  
-              </BooksFormBlock>
-              <BooksFormBlock>
-                <label htmlFor="recommends">こんな人におすすめ！</label>
-                <input type="text" name="recommends" id="nickname" />  
-              </BooksFormBlock>
-              <BooksFormBlock>
-                <label htmlFor="description">内容要約</label>
-                <textarea type="text" name="description" id="nickname"></textarea> 
-              </BooksFormBlock>
-              <BooksFormBlock>
-                <label htmlFor="author">著者</label>
-                <input type="text" name="author" id="nickname" />  
-              </BooksFormBlock>
-              <BooksFormBlock>
-                <label htmlFor="author_kana">著者(カナ)</label>
-                <input type="text" name="author_kana" id="nickname" />  
-              </BooksFormBlock>
-              <BooksFormBlock>
-                <label htmlFor="publisher_name">出版社</label>
-                <input type="text" name="publisher_name" id="nickname" />  
-              </BooksFormBlock>
-              <BooksFormBlock>
-                <label htmlFor="genre_id">ジャンル</label>
-                <input type="text" name="genre_id" id="nickname" />  
-              </BooksFormBlock>
-              <BooksFormBlock>
-                <label htmlFor="sales_date">発売日</label>
-                <input type="text" name="sales_date" id="nickname" />  
-              </BooksFormBlock>
-              <BooksFormBlock>
-                <label htmlFor="item_price">価格</label>
-                <input type="text" name="item_price" id="nickname" />  
-              </BooksFormBlock>
-              <BooksFormBlock>
-                <label htmlFor="image_url">画像</label>
-                <input type="hidden" name="image_url" id="nickname" />  
-              </BooksFormBlock>
-              <BooksFormBlock>
-                <input type="hidden" name="isbn" id="nickname" />  
-              </BooksFormBlock>
-              <BooksFormBlock>
-                <input type="submit" value="推薦図書に追加" id="submit-btn"/>
-              </BooksFormBlock>
-            </form>
+            <SearchBookForm search={this.searchBook}/>
           </NewBooksWrapper>
         </ModalContent>
       </ModalOverlay>
