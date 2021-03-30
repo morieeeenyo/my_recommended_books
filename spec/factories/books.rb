b3e3f184133bc7234a8bd240774c4e9f8fe511e6
togo-mentor <uint2435@gmail.com>
2021-03-31 08:21:01 +0900

    fix: remove columns of facoty

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -7,9 +7,6 @@ FactoryBot.define do
     publisher_name {'株式会社test'}
     sales_date {'1996-12-22'}
     item_price {1500}
-    genre_id {10000}
     item_url {"https://books.rakuten.co.jp/hoge/12345"}
-    description {Faker::Lorem.sentence}
-    recommends {Faker::Lorem.sentence}
   end
 end
