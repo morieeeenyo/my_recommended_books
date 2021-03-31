aa9282f0f990eb399979dac122a13add3fd136f1
togo-mentor <uint2435@gmail.com>
2021-04-01 06:21:27 +0900

    refactor: change sign_in code into support module

--- spec/system/books_spec.rb
+++ spec/system/books_spec.rb
@@ -10,17 +10,7 @@ RSpec.describe "Books", type: :system do
   describe "書籍の投稿" do
     context "投稿に成功" do
       before do
-        user.save    
-        visit root_path
-        find('a', text: 'ログイン').click #href属性がないaタグはclick_link, click_onで検出できないのでfindで検出する
-        expect(page).to have_content 'SignIn' 
-        fill_in "email",	with: user.email
-        fill_in "password",	with: user.password
-        click_button "SignIn"
-        sleep 2 #sleepしないと間に合わない
-        # ログインすると表示が切り替わる
-        expect(page).to  have_content 'ログアウト'
-        expect(page).to  have_content 'マイページ'
+        sign_in(user)
       end
 
       it "検索結果から書籍を選択すると書籍の投稿ができる" do
