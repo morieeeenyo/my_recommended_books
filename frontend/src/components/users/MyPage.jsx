ecf4fe77ac9015bea70036f6c95b8739f90a82ad
togo-mentor <uint2435@gmail.com>
2021-04-02 15:23:25 +0900

    style: display link in sidebar of mypage

--- frontend/src/components/users/MyPage.jsx
+++ frontend/src/components/users/MyPage.jsx
@@ -4,6 +4,9 @@ import styled from 'styled-components';
 // コンポーネントの読み込み
 import {Wrapper} from "../common/Container.jsx"
 
+// react-routerの読み込み
+import { Link } from "react-router-dom";
+
 
 class MyPage extends React.Component {
   render () {
@@ -14,6 +17,18 @@ class MyPage extends React.Component {
         </MyPageHeader>
         <MyPageMain>
           <MyPageSideBar>
+            <Link>
+              推薦図書一覧
+            </Link>
+            <Link>
+              推薦図書一覧
+            </Link>
+            <Link>
+              推薦図書一覧
+            </Link>
+            <Link>
+              推薦図書一覧
+            </Link>
           </MyPageSideBar>
           <MyPageMainContent>
           </MyPageMainContent>
@@ -30,8 +45,23 @@ const MyPageWrapper = styled(Wrapper) `
 const MyPageHeader = styled.h2`
 `
 const MyPageMain = styled.div`
+  display: flex;
 `
 const MyPageSideBar = styled.div`
+  width: 15%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & a {
+    display: inline-block;
+    padding: 5px;
+    border: 1px solid black;
+    border-radius: 2px;
+    text-decoration: none;
+    color: #000;
+    margin: 5px;
+  }
 `
 
 const MyPageMainContent = styled.div`
