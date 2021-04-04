d3bf9f0b4168271e5f05a5e3d6272573742da669
togo-mentor <uint2435@gmail.com>
2021-04-05 08:07:43 +0900

    test: refactor: allign examples

--- spec/requests/mypages_spec.rb
+++ spec/requests/mypages_spec.rb
@@ -1,19 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Mypages", type: :request do
-  let(:user) { create(:user) }
+  let(:user) { build(:user) }
+  let(:user_book) { build(:user_book) }
+
   describe "マイページの表示" do
-    context "リクエストに成功する時" do
+    context "書籍情報が紐付いていない場合" do
+      before do
+        user.save
+      end
+      
       it "ヘッダーにuidがあればリクエストに成功する" do
         get api_v1_mypage_path(user.id), headers: { uid: user.uid }
         expect(response).to have_http_status(200)
-      end
-
-      it "ヘッダーにuidがあれば正しく書籍情報がレスポンスとして返却される" do
-        user_book = create(:user_book)
-        get api_v1_mypage_path(user.id), headers: { uid: user_book.user.uid }
-        json = JSON.parse(response.body) 
-        expect(json['books'][0]['title']).to eq user_book.book.title
       end
 
       it "ヘッダーにuidがあれば正しくユーザー情報がレスポンスとして返却される" do
@@ -21,6 +20,39 @@ RSpec.describe "Mypages", type: :request do
         json = JSON.parse(response.body) 
         expect(json['user']['uid']).to eq user.uid
       end
+
+      it "ヘッダーにuidがあれば正しくユーザー情報がレスポンスとして返却される" do
+        get api_v1_mypage_path(user.id), headers: { uid: user.uid }
+        json = JSON.parse(response.body) 
+        expect(json['books'].length).to eq 0 
+      end
+
+      
     end
+    
+    context "書籍が投稿済みの場合" do
+      before do
+        user_book.save
+      end
+      
+      it "ヘッダーにuidがあればリクエストに成功する" do
+        get api_v1_mypage_path(user_book.user.id), headers: { uid: user_book.user.uid } #紐付けた中からuserを取り出す
+        expect(response).to have_http_status(200)
+      end
+
+      it "ヘッダーにuidがあれば正しく書籍情報がレスポンスとして返却される" do
+        get api_v1_mypage_path(user_book.user.id), headers: { uid: user_book.user.uid } #紐付けた中からuserを取り出す
+        json = JSON.parse(response.body) 
+        expect(json['books'][0]['title']).to eq user_book.book.title #booksは配列なので添字を使う
+      end
+
+      it "ヘッダーにuidがあれば正しくユーザー情報がレスポンスとして返却される" do
+        get api_v1_mypage_path(user_book.user.id), headers: { uid: user_book.user.uid } #紐付けた中からuserを取り出す
+        json = JSON.parse(response.body) 
+        expect(json['user']['uid']).to eq user_book.user.uid
+      end
+      
+    end
+    
   end
 end
