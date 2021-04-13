525c4fd1f6598f96a9f1b0e4a60e2c54aef7b785
togo-mentor <uint2435@gmail.com>
2021-04-13 19:10:12 +0900

    feat: define create action

--- app/controllers/api/v1/outputs_controller.rb
+++ app/controllers/api/v1/outputs_controller.rb
@@ -2,8 +2,13 @@ module Api
   module V1
     class OutputsController < ApplicationController
       def create
-        
-
+        @book = Book.find(params[:book_id])
+        @output = Output.new(output_params)
+        if @output.valid?
+          @output.save
+        else
+          render json: { errors: @output.errors.full_messages }
+        end
       end
     end
   end
