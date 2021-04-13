78b98320978c76a5f89221503c1a50c19a3392e6
togo-mentor <uint2435@gmail.com>
2021-04-13 19:23:52 +0900

    feat: create factory of output model

---
+++ spec/factories/outputs.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :output do
+    content {'もしかして読書によって人生を変えられるのではないか'}
+    time_of_execution {'寝る前'}
+    what_to_do {'kindleを開く'}
+    how_to_do {'横に紙を置いてメモ書きしながら'}
+  end
+end
