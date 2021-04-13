22e25d52051877cbf0ca3dadb6d7e7578770a462
togo-mentor <uint2435@gmail.com>
2021-04-13 19:03:03 +0900

    fix: rename column of awareness

--- db/migrate/20210413093533_create_action_plans.rb
+++ db/migrate/20210413093533_create_action_plans.rb
@@ -1,9 +1,9 @@
 class CreateActionPlans < ActiveRecord::Migration[6.0]
   def change
     create_table :action_plans do |t|
-      t.datetime :when
-      t.string :what
-      t.string :how
+      t.string :time_of_execution
+      t.string :what_to_do
+      t.string :how_to_do
       t.references :awareness
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_27_062451) do
+ActiveRecord::Schema.define(version: 2021_04_13_093533) do
+
+  create_table "action_plans", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "time_of_execution"
+    t.string "what_to_do"
+    t.string "how_to_do"
+    t.bigint "awareness_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["awareness_id"], name: "index_action_plans_on_awareness_id"
+  end
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +41,14 @@ ActiveRecord::Schema.define(version: 2021_03_27_062451) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "awarenesses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "content"
+    t.bigint "book_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_awarenesses_on_book_id"
   end
 
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
