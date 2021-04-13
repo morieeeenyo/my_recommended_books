cf0891f406c147dad33f9431d3f97ff993911595
togo-mentor <uint2435@gmail.com>
2021-04-14 07:10:03 +0900

    feat: merge user_id

--- app/controllers/api/v1/outputs_controller.rb
+++ app/controllers/api/v1/outputs_controller.rb
@@ -17,7 +17,7 @@ module Api
       private 
 
       def output_params 
-        params.require(:output).permit(:content, :time_of_execution, :what_to_do, :how_to_do).merge(book_id: params[:book_id])
+        params.require(:output).permit(:content, :time_of_execution, :what_to_do, :how_to_do).merge(book_id: params[:book_id], user_id: @user.id)
       end
 
       def user_authentification
