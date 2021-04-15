c6b9eaff354389338fb475dfdbf28c3dd29b8f37
togo-mentor <uint2435@gmail.com>
2021-04-15 09:41:49 +0900

    test: fix: apply to validation

--- spec/factories/outputs.rb
+++ spec/factories/outputs.rb
@@ -2,10 +2,8 @@
 
 FactoryBot.define do
   factory :output do
-    content {'もしかして読書によって人生を変えられるのではないか'}
-    time_of_execution {'寝る前'}
-    what_to_do {'kindleを開く'}
-    how_to_do {'横に紙を置いてメモ書きしながら'}
+    awareness {{content: 'test'}}
+    action_plans {[{time_of_execution: 'test', what_to_do: 'test', how_to_do: ''}]}
     # user_idとbook_idはテストコード上で紐付ける
   end
 end
--- spec/models/output_spec.rb
+++ spec/models/output_spec.rb
@@ -17,17 +17,17 @@ RSpec.describe Output, type: :model do
 
     context "保存に失敗する時" do
       it "気づきの内容が空欄のとき保存できない" do
-        output.content = ""
+        output.awareness[:content] = ""
         output.valid?
         expect(output.errors.full_messages).to include "Content can't be blank"
       end
       it "アクションプランの何をやるか、が空欄のとき保存できない" do
-        output.what_to_do = ""
+        output.action_plans[0][:what_to_do] = ""
         output.valid?
         expect(output.errors.full_messages).to include "What to do can't be blank"
       end
       it "アクションプランのいつやるか、が空欄のとき保存できない" do
-        output.time_of_execution = ""
+        output.action_plans[0][:time_of_execution] = ""
         output.valid?
         expect(output.errors.full_messages).to include "Time of execution can't be blank"
       end
