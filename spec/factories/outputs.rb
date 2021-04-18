83f7908075b8675327e9dc2f6e1cf91168f3984d
togo-mentor <uint2435@gmail.com>
2021-04-19 07:49:39 +0900

    test: refactor: roop test

--- spec/factories/outputs.rb
+++ spec/factories/outputs.rb
@@ -3,7 +3,11 @@
 FactoryBot.define do
   factory :output do
     content {'test'}
-    action_plans {[{time_of_execution: 'test', what_to_do: 'test', how_to_do: ''}]}
+    action_plans = []
+    3.times do 
+      action_plans << {time_of_execution: 'test', what_to_do: 'test', how_to_do: 'test'}
+    end
+    action_plans { action_plans }
     # user_idとbook_idはテストコード上で紐付ける
   end
 end
--- spec/models/output_spec.rb
+++ spec/models/output_spec.rb
@@ -11,7 +11,11 @@ RSpec.describe Output, type: :model do
         expect(output).to  be_valid
       end
       it "どのように実践するか、は空でも保存できる" do
-        expect(output).to  be_valid
+        output.action_plans.each_with_index  do |action_plan, index|
+          output.action_plans[index][:how_to_do] = ""
+          expect(output).to  be_valid
+          output.action_plans[index][:how_to_do] = "test" #これがないと空にした値が引き継がれてしまう
+        end
       end
     end
 
@@ -22,14 +26,20 @@ RSpec.describe Output, type: :model do
         expect(output.errors.full_messages).to include "Content can't be blank"
       end
       it "アクションプランの何をやるか、が空欄のとき保存できない" do
-        output.action_plans[0][:what_to_do] = ""
-        output.valid?
-        expect(output.errors.full_messages).to include "What to do can't be blank"
+        output.action_plans.each_with_index do |action_plan, index|
+          output.action_plans[index][:what_to_do] = ""
+          output.valid?
+          expect(output.errors.full_messages).to include "What to do of action plan #{index + 1} can't be blank"
+          output.action_plans[index][:what_to_do] = "test" #これがないと空にした値が引き継がれてしまう
+        end
       end
       it "アクションプランのいつやるか、が空欄のとき保存できない" do
-        output.action_plans[0][:time_of_execution] = ""
-        output.valid?
-        expect(output.errors.full_messages).to include "Time of execution can't be blank"
+        output.action_plans.each_with_index do |action_plan, index|
+          output.action_plans[index][:time_of_execution] = ""
+          output.valid?
+          expect(output.errors.full_messages).to include "Time of execution of action plan #{index + 1} can't be blank"
+          output.action_plans[index][:time_of_execution] = "test" #これがないと空にした値が引き継がれてしまう
+        end
       end
       it "ユーザーが存在しないとき保存できない" do
         output.user_id = ""
