7ef755538d8ace089e758ab8454833db4dccd8e1
togo-mentor <uint2435@gmail.com>
2021-04-19 08:01:35 +0900

    test: refactor: roop test

--- spec/requests/outputs_spec.rb
+++ spec/requests/outputs_spec.rb
@@ -16,47 +16,57 @@ RSpec.describe "Outputs", type: :request do
       end
 
       it "すべてのカラムが揃っていればレスポンスで気づきとアクションプランが得られる" do
-        post api_v1_book_outputs_path(book.id), xhr: true, params: {output: output_params}, headers: headers
-        sleep 1 #sleepしないとレスポンスの返却が間に合わない
-        json = JSON.parse(response.body)
-        # アクションプランと気付きは別々にレスポンスとして返却される
-        expect(json['awareness']['content']).to eq output_params[:content]
-        expect(json['action_plans'][0]['what_to_do']).to eq output_params[:action_plans][0][:what_to_do]
+        output_params[:action_plans].each_with_index do |action_plan, index|
+          post api_v1_book_outputs_path(book.id), xhr: true, params: {output: output_params}, headers: headers
+          sleep 1 #sleepしないとレスポンスの返却が間に合わない
+          json = JSON.parse(response.body)
+          # アクションプランと気付きは別々にレスポンスとして返却される
+          expect(json['awareness']['content']).to eq output_params[:content]
+          expect(json['action_plans'][index]['what_to_do']).to eq output_params[:action_plans][index][:what_to_do]
+        end
       end
     end
     
     context "投稿に成功する時(任意項目が空)" do
-      before do
-        output_params[:how_to_do] = "" #どのように実践するか、は空欄でOK
-      end
-      
       it "投稿に成功するとステータスが201で返却される" do
-        post api_v1_book_outputs_path(book.id), xhr: true, params: {output: output_params}, headers: headers
-        expect(response).to have_http_status(201) #リソース保存時のステータスは201
+        output_params[:action_plans].each_with_index do |action_plan, index|
+          action_plan[:how_to_do] = "" #どのように実践するか、は空欄でOK
+          post api_v1_book_outputs_path(book.id), xhr: true, params: {output: output_params}, headers: headers
+          expect(response).to have_http_status(201) #リソース保存時のステータスは201
+        end
       end
   
       it "すべてのカラムが揃っていればレスポンスで気づきとアクションプランが得られる" do
-        post api_v1_book_outputs_path(book.id), xhr: true, params: {output: output_params}, headers: headers
-        sleep 1 #sleepしないとレスポンスの返却が間に合わない
-        json = JSON.parse(response.body)
-        expect(json['awareness']['content']).to eq output_params[:content]
-        expect(json['action_plans'][0]['what_to_do']).to eq output_params[:action_plans][0][:what_to_do]
+        output_params[:action_plans].each_with_index do |action_plan, index|
+          action_plan[:how_to_do] = "" #どのように実践するか、は空欄でOK
+          post api_v1_book_outputs_path(book.id), xhr: true, params: {output: output_params}, headers: headers
+          sleep 1 #sleepしないとレスポンスの返却が間に合わない
+          json = JSON.parse(response.body)
+          expect(json['awareness']['content']).to eq output_params[:content]
+          expect(json['action_plans'][index]['what_to_do']).to eq output_params[:action_plans][index][:what_to_do]
+        end
       end   
     end
     
     context "投稿に失敗する時" do
       # 個別のバリデーションの検証はmodel_psecにて。バリデーションはすべてpresence: true
       it "必須のカラムが不足している時保存に失敗しステータスが404になる" do
-        output_params[:action_plans][0][:what_to_do] = ""
-        post api_v1_book_outputs_path(book.id), xhr: true, params: {output: output_params}, headers: headers
-        expect(response).to have_http_status(422)
+        output_params[:action_plans].each_with_index do |action_plan, index|
+          output_params[:action_plans][index][:what_to_do] = ""
+          post api_v1_book_outputs_path(book.id), xhr: true, params: {output: output_params}, headers: headers
+          expect(response).to have_http_status(422)
+          output_params[:action_plans][index][:what_to_do] = "test" #これがないと空にした値が引き継がれてしまう
+        end
       end
 
       it "保存に失敗した時エラーメッセージがレスポンスとして返却される" do
-        output_params[:action_plans][0][:what_to_do] = ""
-        post api_v1_book_outputs_path(book.id), xhr: true, params: {output: output_params}, headers: headers
-        json = JSON.parse(response.body)
-        expect(json['errors']).to include "What to do can't be blank"
+        output_params[:action_plans].each_with_index do |action_plan, index|
+          output_params[:action_plans][index][:what_to_do] = ""
+          post api_v1_book_outputs_path(book.id), xhr: true, params: {output: output_params}, headers: headers
+          json = JSON.parse(response.body)
+          expect(json['errors']).to include "What to do of action plan #{index + 1} can't be blank"
+          output_params[:action_plans][index][:what_to_do] = "test" #これがないと空にした値が引き継がれてしまう
+        end
       end
     end
   end
