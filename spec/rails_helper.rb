c1ed87453236da8cc9b9541ac280b60d977c6311
togo-mentor <uint2435@gmail.com>
2021-04-20 10:00:41 +0900

    fix: useless variable deleted

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,13 +35,13 @@ end
 
 Capybara.register_driver :remote_chrome do |app|
   options = ::Selenium::WebDriver::Chrome::Options.new
-  
+
   options.add_argument('--headless')
   options.add_argument('--no-sandbox')
   options.add_argument('--disable-dev-shm-usage')
   options.add_argument('--window-size=1400,1400')
 
-  driver = Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
 Capybara.javascript_driver = :chrome_headless
