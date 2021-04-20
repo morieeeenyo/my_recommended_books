ff1d68b21455fcba13835c6fc9197d2dc9791602
togo-mentor <uint2435@gmail.com>
2021-04-20 09:59:05 +0900

    fix: change setting of capybara

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,8 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 
 Capybara.register_driver :remote_chrome do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+  
   options.add_argument('--headless')
   options.add_argument('--no-sandbox')
   options.add_argument('--disable-dev-shm-usage')
