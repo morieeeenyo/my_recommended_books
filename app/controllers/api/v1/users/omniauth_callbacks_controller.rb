dc2c86c2a91cd8205604d557087337738a64b378
togo-mentor <uint2435@gmail.com>
2021-04-27 18:01:38 +0900

    test: valid case of user oauthentification

--- app/controllers/api/v1/users/omniauth_callbacks_controller.rb
+++ app/controllers/api/v1/users/omniauth_callbacks_controller.rb
@@ -4,24 +4,82 @@ module Api
       class OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksController
 
         
-        def twitter
-          callback_from :twitter
+        def redirect_callbacks
+          devise_mapping = getting_devise_mapping
+          redirect_route = get_redirect_route(devise_mapping)
+  
+          session["dta.omniauth.auth"] = request.env["omniauth.auth"].except("extra")
+          session["dta.omniauth.params"] = request.env["omniauth.params"]
+          redirect_to redirect_route
         end
-      
-        private
-      
-        def callback_from(provider)
-          provider = provider.to_s
-      
-          @user = User.find_for_oauth(request.env['omniauth.auth'])
-      
-          if @user.persisted?
-            flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
-            sign_in_and_redirect @user, event: :authentication
+  
+        def omniauth_success
+          gets_resource_from_auth_hash
+          # create_token_info
+          set_token_on_resource
+          create_auth_params
+  
+          sign_in(:user, @resource, store: false, bypass: false)
+  
+        # 動作確認用にユーザ情報を保存できたらjsonをそのまま返す処理
+          if @resource.save!
+            # update_token_authをつけることでレスポンスヘッダーに認証情報を付与できる。
+            update_auth_header
+            yield @resource if block_given?
+            render json: @resource, status: :ok
           else
-            session["devise.#{provider}_data"] = request.env['omniauth.auth']
-            redirect_to new_user_registration_url
+            render json: { message: "failed to login" }, status: 500
           end
+  
+        # 本実装時はこちらを使用する
+        # @resource.save!
+  
+        # update_auth_header # これは自分で追加する
+        # yield @resource if block_given?
+  
+        # render_data_or_redirect('deliverCredentials', @auth_params.as_json, @resource.as_json)
+        end
+  
+        protected
+  
+        def gets_resource_from_auth_hash
+          @resource = resource_class.where({
+            uid: auth_hash["uid"],
+            provider: auth_hash["provider"]
+          }).first_or_initialize
+  
+          if @resource.new_record?
+            @oauth_registration = true
+          end
+  
+          assign_provider_attrs(@resource, auth_hash)
+  
+          extra_params = whitelisted_params
+          @resource.assign_attributes(extra_params) if extra_params
+  
+          @resource
+        end
+  
+        private
+  
+        def get_redirect_route(devise_mapping)
+          path = "#{Devise.mappings[devise_mapping.to_sym].fullpath}/#{params[:provider]}/callback"
+          klass = request.scheme == "https" ? URI::HTTPS : URI::HTTP
+          klass.build(host: request.host, port: request.port, path: path).to_s
+        end
+  
+        def getting_devise_mapping
+          [request.env["omniauth.params"]["namespace_name"], request.env["omniauth.params"]["resource_class"].underscore.tr("/", "_")].compact.join("_")
+        rescue NoMethodError
+          default_devise_mapping
+        end
+  
+        def default_devise_mapping
+          raise NotImplementedError, "no default_devise_mapping set"
+        end
+  
+        def authenticate_api_user!
+          false
         end
 
       end
--- spec/requests/omniauth_users_spec.rb
+++ spec/requests/omniauth_users_spec.rb
@@ -1,10 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "OmniauthUsers", type: :request do
-  describe "GET /omniauth_users" do
-    it "works! (now write some real specs)" do
-      get omniauth_users_path
-      expect(response).to have_http_status(200)
+  # before { request.env["omniauth.auth"] = twitter_mock }
+
+  before do
+    OmniAuth.config.mock_auth[:twitter] = nil
+    Rails.application.env_config['omniauth.auth'] = twitter_mock
+  end
+
+  describe "omniauthを用いたTwitterでのログイン" do
+    context "ログインに成功" do
+      it "oauthのデータが存在する場合ログインに成功する" do
+        get api_v1_users_failure_path
+        expect(response).to have_http_status(200)
+      end
+
+      it "oauthのデータが存在する場合mockのデータに応じたレスポンスが返却される" do
+        
+      end
+    end
+
+    context "ログイン失敗" do
+      
     end
   end
 end
