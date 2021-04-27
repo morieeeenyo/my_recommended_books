0507ceda310e65a19d173415b1eea1f533571b08
togo-mentor <uint2435@gmail.com>
2021-04-27 19:22:20 +0900

    feat: added twitter provide to user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,5 @@ class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   include ActionController::RequestForgeryProtection
   protect_from_forgery with: :exception
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:twitter]
   include DeviseTokenAuth::Concerns::User
 
   validates :nickname, presence: true, uniqueness: { case_sensitive: true }
