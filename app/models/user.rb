02d63748dbd5ea56786d11c351adaf8b685caefc
togo-mentor <uint2435@gmail.com>
2021-04-27 17:11:06 +0900

    feat: added omniauth settings to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
   include DeviseTokenAuth::Concerns::User
 
   validates :nickname, presence: true, uniqueness: { case_sensitive: true }
@@ -19,4 +19,20 @@ class User < ActiveRecord::Base
   has_many :books, through: :user_books
   has_many :awarenesses
   has_many :action_plans
+
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        email:    User.dummy_email(auth),
+        password: Devise.friendly_token[0, 20]
+      )
+      user.save!
+    end
+
+    current_user = user
+  end
 end
