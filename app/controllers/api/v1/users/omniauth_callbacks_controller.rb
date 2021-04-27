c4824acf357eb37c5f282cae8ecafc0efe55fdce
togo-mentor <uint2435@gmail.com>
2021-04-28 07:10:18 +0900

    fix: override moethods to pass test

--- app/controllers/api/v1/users/omniauth_callbacks_controller.rb
+++ app/controllers/api/v1/users/omniauth_callbacks_controller.rb
@@ -53,9 +53,35 @@ module Api
         def set_random_password
           # set crazy password for new oauth users. this is only used to prevent
           # access via email sign-in.
-          p = SecureRandom.alphanumeric(10) + [*'a'..'z'].sample(1).join + [*'0'..'9'].sample(1).join
-          @resource.password = p
-          @resource.password_confirmation = p
+          random_password = SecureRandom.alphanumeric(10) + [*'a'..'z'].sample(1).join + [*'0'..'9'].sample(1).join
+          @resource.password = random_password
+          @resource.password_confirmation = random_password
+        end
+
+        def get_resource_from_auth_hash
+          unless auth_hash
+            auth_hash = request.env['omniauth.auth'] #主にテストコード用。auth_hashを直接定義できないくさいのでrequest.envから取り出す
+          end
+          
+          @resource = resource_class.where(
+            uid: auth_hash['uid'],
+            provider: auth_hash['provider']
+          ).first_or_initialize
+    
+          if @resource.new_record?
+            handle_new_resource
+          end
+    
+          # sync user info with provider, update/generate auth token
+          assign_provider_attrs(@resource, auth_hash)
+    
+          # assign any additional (whitelisted) attributes
+          if assign_whitelisted_params?
+            extra_params = whitelisted_params
+            @resource.assign_attributes(extra_params) if extra_params
+          end
+    
+          @resource
         end
   
       end
