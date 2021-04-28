ef4272ebd18ebd275bffec264fc4402f811af938
togo-mentor <uint2435@gmail.com>
2021-04-28 09:24:04 +0900

    fix: added condition to book post to pass ci

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -15,7 +15,9 @@ module Api
           @book.save
           @user.books << @book # ユーザーと書籍を紐付ける。ここで書籍が投稿済みの場合は中間テーブルにのみデータが入る。
           render status: 201, json: { book: @book } # ステータスは手動で入れないと反映されない。リソース保存時のステータスは201
-          @twitter_client.update!("API連携のテストです。\n『#{@book.title}』を推薦図書に追加しました！ \n #読書 #読書好きとつながりたい #Kaidoku") # アプリURLへの導線を貼る
+          if !Rails.env.test? #一旦テスト時は読み込まない設定にする。次のブランチで詳細実装詰める
+            @twitter_client.update!("API連携のテストです。\n『#{@book.title}』を推薦図書に追加しました！ \n #読書 #読書好きとつながりたい #Kaidoku") # アプリURLへの導線を貼る
+          end
         else
           render status: 422, json: { errors: @book.errors.full_messages } # バリデーションに引っかかった際のステータスは422(Unprocessable entity)
         end
