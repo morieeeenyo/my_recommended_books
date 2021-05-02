8f44377caae0797e764794471c56f134eb7e7673
togo-mentor <uint2435@gmail.com>
2021-05-02 18:54:54 +0900

    fix: set user sns_token and sns_secret

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -58,8 +58,8 @@ module Api
 
       def set_twitter_client
         @twitter_client = Twitter::REST::Client.new do |config|
-          config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
-          config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+          config.access_token        = @user.sns_token
+          config.access_token_secret = @user.sns_secret
           config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
           config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
         end
