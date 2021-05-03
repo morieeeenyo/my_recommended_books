5721ae86f1bc013f14ea241f9240980eaea3929c
togo-mentor <uint2435@gmail.com>
2021-05-04 05:57:56 +0900

    test: valid case of omniauth

--- spec/system/omniauth_users_spec.rb
+++ spec/system/omniauth_users_spec.rb
@@ -17,12 +17,31 @@ RSpec.describe "OmniauthUsers", type: :system do
         end.to change(User, :count).by(1)
       end
 
+      it "初回ログイン時はCookieに認証情報と初回ログインを示すtmpデータが保存される" do        
+        click_link 'SignUp with Twitter'
+        cookies = page.driver.browser.manage.all_cookies
+        authToken = cookies.find { |c| c[:name] == 'authToken' }
+        expect(authToken[:value]).not_to eq nil        
+        first_session = cookies.find { |c| c[:name] == 'first_session' }
+        expect(first_session[:value]).to eq 'true'
+      end
+      
       it "ユーザーが認証済みの場合ユーザーのカウントは増えない", js: true do
         # 本来は2回リンク踏ませるのが正しいがなんかうまくいかないので代わりに事前にユーザーを作っておく
         create(:user, uid: twitter_mock['uid'], provider: twitter_mock['provider'])
         expect do
           find('a', text: 'SignUp with Twitter').click # reactで作ったaタグはhref属性がつかないのでfindで検出する
         end.not_to change(User, :count)
+      end
+
+      it "ユーザーが認証済みの場合時はCookieに認証情報のみが保存される" do        
+        create(:user, uid: twitter_mock['uid'], provider: twitter_mock['provider'])
+        click_link 'SignUp with Twitter'
+        cookies = page.driver.browser.manage.all_cookies
+        authToken = cookies.find { |c| c[:name] == 'authToken' }
+        expect(authToken[:value]).not_to eq nil        
+        first_session = cookies.find { |c| c[:name] == 'first_session' }
+        expect(first_session).to eq nil
       end
     end
   end
