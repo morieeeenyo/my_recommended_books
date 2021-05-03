e5e041d1c2ad77af6071cbafd37231f5179fa54a
togo-mentor <uint2435@gmail.com>
2021-05-03 17:58:29 +0900

    test: valid case of oauth

--- spec/system/omniauth_users_spec.rb
+++ spec/system/omniauth_users_spec.rb
@@ -6,28 +6,23 @@ RSpec.describe "OmniauthUsers", type: :system do
     OmniAuth.config.mock_auth[:twitter] = nil # テストごとに認証情報を初期化する
     Rails.application.env_config['omniauth.auth'] = twitter_mock
     visit root_path
+    click_link '新規登録'
   end
 
   describe "Twitter認証ログイン" do
     context "認証に成功する" do
-      it "初回ログイン時はユーザーのカウントが1増える" do
-        
+      it "初回ログイン時はユーザーのカウントが1増える" do        
+        expect do 
+          click_link 'SignUp with Twitter'
+        end.to change(User, :count).by(1)
       end
 
-      it "2回目以降のログイン時にはユーザーのカウントは増えない" do
-        
+      it "ユーザーが認証済みの場合ユーザーのカウントは増えない", js: true do
+        create(:user, uid: twitter_mock['uid'], provider: twitter_mock['provider'])
+        expect do
+          find('a', text: 'SignUp with Twitter').click # reactで作ったaタグはhref属性がつかないのでfindで検出する
+        end.not_to change(User, :count)
       end
-      
-      
-      
-    end
-
-    context "認証に失敗する" do
-      it "request.env['omniauth.auth']がnilの場合エラーが発生する" do
-        
-      end
-      
-      
     end
   end
 end
