e9f4ad38d09c1dacbfd9c468e1c501d5ebdddf01
togo-mentor <uint2435@gmail.com>
2021-05-13 08:06:58 +0900

    style: markuped search field

--- frontend/src/components/books/Index.jsx
+++ frontend/src/components/books/Index.jsx
@@ -28,13 +28,84 @@ class Index extends React.Component {
 
   render () {
     return (
-      <div className="container">
-    
-      </div>
+      <BookIndexContainer>
+        <div className="search">
+          <h2>書籍検索</h2>
+          <p>気になる本があれば検索してみましょう。<br></br>すでに読んだ方のアウトプットが見つかるかもしれません。</p>
+          <div className="search-form-field">
+            <input type="text" placeholder="書籍名で検索" ></input>
+            <button className="search-button"><i className="fas fa-search"></i></button>  
+          </div>
+        </div>
+
+        <div className="book_list">
+          <h2>新着書籍一覧</h2>
+        </div>
+      </BookIndexContainer>
      )
    
   } 
 }
 
+const BookIndexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 80%;
+  margin: 0 auto;
+
+  & div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 80%;
+    margin: 0 auto;
+
+    & h2 {
+      font-size: 32px;
+    }
+
+    & p {
+      text-align: center;
+    }
+  }
+
+  & .search-form-field {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center; 
+    width: 40%;
+
+    & input {
+      height: 24px;
+      width: 82%;
+      padding: 10px;
+    }
+
+    & .search-button {
+      /* 検索ボタン */
+      vertical-align: center;
+      position: static;
+      background-color: #989EAB;
+      color: #F4F5F7;
+      height: 50px;
+      padding: 0 15px;
+      border-style: none;
+
+      & i {
+        font-size: 16px;
+      }
+    }
+
+    & .search-button:hover {
+      cursor: pointer;
+      color: #535F78;
+      background-color: #F4F5F7;
+      border: 1px solid #989EAB;
+    }
+
+  }
+`
 
 export default withRouter(Index);
