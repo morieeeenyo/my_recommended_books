794eea1a6763981ae56bf3459c54699945e930d1
togo-mentor <uint2435@gmail.com>
2021-05-12 09:22:33 +0900

    feat: def index

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -7,6 +7,11 @@ module Api
       # ユーザーが認証済みかどうかチェック
       before_action :set_twitter_client, only: :create
 
+      def index
+        # アウトプット投稿数が多い順にソート
+        render json: { books: Books.includes(:awarenesses).order('count(book_id) DESC') }
+      end
+
       def create
         # ユーザー認証に引っかかった際のステータスは401(Unautorized)
         return render status: 401, json: { errors: '推薦図書の投稿にはログインが必要です' } unless @user && @token && @client
