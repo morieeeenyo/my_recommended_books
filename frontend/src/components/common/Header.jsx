759a2be56cf7846c373ca0e91714fe4f37c011fc
togo-mentor <uint2435@gmail.com>
2021-05-14 06:44:02 +0900

    fix: header logo path change to root

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -27,7 +27,7 @@ class Header extends React.Component {
           <HeaderContainer>
             {this.props.children}
             <HeaderTitle>
-              <Link to="/welcome">
+              <Link to="/">
                 <img src={Logo} alt="Kaidoku" width="65" height="65"/> {/* ロゴの高さはヘッダーより5pxだけ小さい */}
               </Link>
             </HeaderTitle>
