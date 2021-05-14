fededc0d30d7f9cb1037bf1de2d77a38d317a0b7
togo-mentor <uint2435@gmail.com>
2021-05-15 06:40:56 +0900

    feat: import pagenation

--- frontend/src/components/books/Index.jsx
+++ frontend/src/components/books/Index.jsx
@@ -7,17 +7,31 @@ import { Link, withRouter } from "react-router-dom";
 // Cookieの読み込み。localStorageを使用せずCookieを使用する方針に切り替え
 import Cookies from 'universal-cookie';
 
-//axiosの読み込み
+// axiosの読み込み
 import axios from 'axios';
 
+// コンポーネントの読み込み
 import {BookList} from '../users/MyPage.jsx'
+
+// paginateの読み込み
+import ReactPaginate from 'react-paginate';
 
 class Index extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      books: []
+      books: [],
+      start: 0,
+      perPage: 12
     }
+    this.pageChange = this.pageChange.bind(this)
+  }
+
+  pageChange(data) {
+    let pageNumber = data['selected'];
+    this.setState({
+      start: pageNumber * this.state.perPage
+    })
   }
 
   componentDidMount() {
@@ -38,6 +52,7 @@ class Index extends React.Component {
     } 
 
     // 書籍投稿ボタンが非表示の場合表示する
+    // else文だとうまく作動しないためauthTokenがあるかどうかで分ける
     if (authToken) {
       if (newBookLink.getAttribute('style') == 'display: none;') {
         newBookLink.setAttribute('style', 'display: block;')
@@ -68,10 +83,10 @@ class Index extends React.Component {
           </div>
         </div>
 
-        <div className="book_list">
+        <div className="book-list">
           <h2>新着書籍一覧</h2>
           <BookList>
-            {this.state.books.slice(0,12).map(book => {
+            {this.state.books.slice(this.state.start, this.state.start + this.state.perPage).map(book => {
               return (
               <li key={book.isbn} className="book-list-item">
                 <img src={book.image_url}/>
@@ -82,6 +97,26 @@ class Index extends React.Component {
               ) 
             })} 
           </BookList>
+          <ReactPaginate
+            pageCount={Math.ceil(this.state.books.length / this.state.perPage)}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={this.pageChange}
+            containerClassName='pagination'
+            pageClassName='page-item'
+            pageLinkClassName='page-link'
+            activeClassName='active'
+            previousLabel='<'
+            nextLabel='>'
+            previousClassName='page-item'
+            nextClassName='page-item'
+            previousLinkClassName='page-link'
+            nextLinkClassName='page-link'
+            disabledClassName='disabled'
+            breakLabel='...'
+            breakClassName='page-item'
+            breakLinkClassName='page-link'
+          />
         </div>
       </BookIndexContainer>
      )
@@ -146,7 +181,69 @@ const BookIndexContainer = styled.div`
       background-color: #F4F5F7;
       border: 1px solid #989EAB;
     }
+  }
 
+  & .book-list {
+    
+    & .pagination {
+      display: flex;
+      justify-content: space-between;
+      margin: 0 auto;
+      width: 20%;
+      padding: 0;
+
+      & > li {
+          list-style: none;
+          margin: 0 12px;
+
+          & > a {
+              position: relative;
+              font-size: 12px;
+              outline: none;
+              z-index: 100;
+              cursor: pointer;
+              color: #000;
+
+              &::before {
+                content: "";
+                display: block;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                width: 24px;
+                height: 24px;
+                border-radius: 50%;
+                transform: translate(-50%, -50%);
+                z-index: -100;
+              }
+
+              &:hover {
+                  font-weight: bold;
+                  
+                  &::before {
+                      background-color: #fff;
+                  }
+              }
+          }
+
+          &.active {
+              & > a::before {
+                  background-color: $white;
+              }
+          }
+      }
+
+      &__previous,
+      &__next {
+          & > a {
+              font-size: 0.7rem;
+          }
+      }
+
+      &__disabled {
+          display: none;
+      }
+    }
   }
 `
 
