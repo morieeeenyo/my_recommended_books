7ae33f286e403e1cc8c2ee0516fdfe517e85b9bb
togo-mentor <uint2435@gmail.com>
2021-05-18 07:58:17 +0900

    test: success to pass output index examples

--- spec/requests/outputs_spec.rb
+++ spec/requests/outputs_spec.rb
@@ -3,12 +3,108 @@ require 'rails_helper'
 RSpec.describe 'Outputs', type: :request do
   let(:user) { create(:user) }
   let(:book) { create(:book) }
+  let(:another_book) { create(:book) }
+  let(:user_book_relation) { create(:user_book, book_id: book.id, user_id: user.id) }
+  let(:user_another_book_relation) { build(:user_book, book_id: another_book.id, user_id: user.id) }
+  let(:another_user_book_relation) { create(:user_book, book_id: book.id) }
+  let(:output) { build(:output, book_id: user_book_relation.book.id, user_id: user_book_relation.user.id) }
+  let(:output_of_others) { build(:output, book_id: another_user_book_relation.book.id, user_id: another_user_book_relation.user.id) }
   let(:output_params) { attributes_for(:output, user_id: user.id, book_id: book.id) }
   let(:headers) do
     { 'uid' => user.uid, 'access-token' => 'ABCDEFGH12345678', 'client' => 'H-12345678' } # ユーザー認証用ヘッダ
   end
 
   # 異常形は3つ中1つ空のときのみ検証。それ以外のパターンはsystem_specにて
+
+  describe "アウトプット一覧" do
+    context "アウトプットが1件以上投稿されている場合" do
+      before do
+        3.times do 
+          output_of_others.save
+        end
+      end
+      
+      it "ログイン中のユーザーがアウトプットを投稿していない場合ステータスが200になる" do
+        get api_v1_book_outputs_path(book.isbn), xhr: true, headers: headers
+        expect(response).to have_http_status(200)
+      end
+
+      it "ログイン中のユーザーがアウトプットを投稿していない場合すべてのアウトプットが新しい順に返却される" do
+        get api_v1_book_outputs_path(book.isbn), xhr: true, headers: headers
+        sleep 2 # sleepしないとレスポンスの返却が間に合わない
+        json = JSON.parse(response.body)
+        sleep 2
+        expect(json['myoutputs']).to eq nil # ログイン中のユーザーがアウトプットを投稿していれば返却されるデータ
+        expect(json['outputs'].length).to eq 3
+        # 添字が若いほどidの値が大きい=新しい順
+        expect(json['outputs'][0]['awareness']['id']).to be > json['outputs'][1]['awareness']['id']
+        expect(json['outputs'][1]['awareness']['id']).to be > json['outputs'][2]['awareness']['id']
+      end
+      
+      it "ログイン中のユーザーがアウトプットを投稿している場合、ステータスが200になる" do
+        # 3個保存することで複数データの取得が可能かどうか、順番は正しいかを検証
+        2.times do
+          output.save
+        end
+        get api_v1_book_outputs_path(book.isbn), xhr: true, headers: headers
+        expect(response).to have_http_status(200)
+      end
+
+      it "ログイン中のユーザーがアウトプットを投稿している場合、自分が投稿したアウトプットと他の人が投稿したアウトプットが別々に返却される" do
+        # 3個保存することで複数データの取得が可能かどうか、順番は正しいかを検証
+        2.times do
+          output.save
+        end
+        get api_v1_book_outputs_path(book.isbn), xhr: true, headers: headers
+        sleep 2 # sleepしないとレスポンスの返却が間に合わない
+        json = JSON.parse(response.body)
+        sleep 2
+        expect(json['myoutputs'].length).to eq 2
+        # 添字が若いほどidの値が大きい=新しい順
+        expect(json['myoutputs'][0]['awareness']['id']).to be > json['myoutputs'][1]['awareness']['id']
+        expect(json['outputs'].length).to eq 3
+        # 添字が若いほどidの値が大きい=新しい順
+        expect(json['outputs'][0]['awareness']['id']).to be > json['outputs'][1]['awareness']['id']
+        expect(json['outputs'][1]['awareness']['id']).to be > json['outputs'][2]['awareness']['id']    
+      end
+      
+    end
+
+    context "アウトプットが0件の場合" do
+      before do
+        Awareness.delete_all # letを用いてすでに投稿されたアウトプットを削除
+      end
+      
+      it "ログイン中のユーザーが推薦図書に追加していない場合ステータスは200" do
+        get api_v1_book_outputs_path(another_book.isbn), xhr: true, headers: headers
+        expect(response).to have_http_status(200)
+      end
+
+      it "ログイン中のユーザーが推薦図書に追加していない場合レスポンスは0件" do
+        get api_v1_book_outputs_path(another_book.isbn), xhr: true, headers: headers
+        sleep 2 # sleepしないとレスポンスの返却が間に合わない
+        json = JSON.parse(response.body)
+        sleep 2
+        expect(json['outputs'].length).to eq 0
+      end
+
+      it "ログイン中のユーザーが推薦図書に追加している場合ステータスは200" do
+        user_another_book_relation.save
+        get api_v1_book_outputs_path(another_book.isbn), xhr: true, headers: headers
+        expect(response).to have_http_status(200)
+      end
+
+      it "ログイン中のユーザーが推薦図書に追加している場合レスポンスが0件" do
+        user_another_book_relation.save
+        get api_v1_book_outputs_path(another_book.isbn), xhr: true, headers: headers
+        sleep 2 # sleepしないとレスポンスの返却が間に合わない
+        json = JSON.parse(response.body)
+        sleep 2
+        expect(json['outputs'].length).to eq 0
+      end
+    end
+  end
+  
 
   describe 'アウトプットの投稿' do
     context '投稿に成功する時' do
