64507d506955033a5efc53d8022fbb5575dbc6df
togo-mentor <uint2435@gmail.com>
2021-06-02 06:42:06 +0900

    fix: move ssh setting to deploy/production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -49,13 +49,10 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: "user_name", # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: "please use keys"
-#   }
+server "13.112.8.7", user: "togo", roles: %w{app db web}
+
+set :ssh_options, {
+  keys: %w(~/.ssh/kaidoku_2.pem),
+  forward_agent: true,
+  auth_methods: %w(publickey),
+}
