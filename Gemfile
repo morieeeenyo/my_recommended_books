6ba385ddd1a91f1f2b2911b770c87f2e1e447b81
togo-mentor <uint2435@gmail.com>
2021-06-07 06:52:32 +0900

    feat: cap webpack on ec2

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ group :development, :test do
   gem 'capistrano-bundler'
   gem 'capistrano-rails'
   gem 'capistrano3-unicorn'
+  gem 'capistrano-npm'
 end
 
 group :development do
--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -2338,8 +2338,9 @@ eval("\n\nvar bind = __webpack_require__(/*! ./helpers/bind */ \"../node_modules
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
 /******/ 		// Check if module is in cache
-/******/ 		if(__webpack_module_cache__[moduleId]) {
-/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
 /******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = __webpack_module_cache__[moduleId] = {
