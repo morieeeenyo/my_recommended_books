d13c0adf1828400686447fd4ed713f85ffa892ed
togo-mentor <uint2435@gmail.com>
2021-06-13 08:13:15 +0900

    fix: meta tags moved

--- frontend/src/components/common/MetaTags.jsx
+++ frontend/src/components/common/MetaTags.jsx
@@ -3,11 +3,11 @@ import { Helmet } from "react-helmet";
 
 import React, { useEffect } from 'react';
 
-export function MetaTags(title, description) {
+export function MetaTags(props) {
   useEffect(() => {
     const headElements = document.head.children
     Array.from(headElements).forEach(el => {
-      if (el.tagName == 'STYLE') {
+      if (el.tagName == 'STYLE' || el.tagName == 'SCRIPT') {
         document.head.appendChild(el)
       }
     })
@@ -15,14 +15,12 @@ export function MetaTags(title, description) {
 
   return(
   <Helmet 
-        meta = {[
-        { name: 'charset', content: 'UTF-8'},
-        { property: 'twitter:card', content: 'summary' },
-        { property: 'og:image', content: "https://kaidoku.s3.ap-northeast-1.amazonaws.com/public/header_logo.png" },
-        { property: 'og:title', content: title },
-        { property: 'og:description', content: description },
-        { property: 'og:url', content: location.href },
-      ]}>    
+      meta = {[
+      { property: 'og:image', content: "https://kaidoku.s3.ap-northeast-1.amazonaws.com/public/header_logo.png" },
+      { property: 'og:title', content: props.title },
+      { property: 'og:description', content: props.description },
+      { property: 'og:url', content: location.href },
+    ]}>    
   </Helmet>
   )
 }
