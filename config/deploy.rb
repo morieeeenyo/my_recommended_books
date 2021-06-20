dddafe2543cb640173cc89e39b49e0b213c8d665
togo-mentor <uint2435@gmail.com>
2021-06-20 17:52:17 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :application, 'my_recommended_books'
 set :repo_url,  'git@github.com:togo-mentor/my_recommended_books.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads', 'public/javascripts')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :linked_files, fetch(:linked_files, []).push("config/master.key")
 
 set :rbenv_type, :user
