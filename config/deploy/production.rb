d86306554d98ad1e0aca50d983dff61a2d10b728
togo-mentor <uint2435@gmail.com>
2021-06-28 05:55:46 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -53,7 +53,7 @@ server "54.250.76.224", user: "togo", roles: %w{app db web}
 
 # CircleCIのGUIで設定した環境変数を使ってSSH接続
 set :ssh_options, {
-  keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
+  keys: %w(~/.ssh/kaidoku_2.pem),
   forward_agent: true,
   auth_methods: %w[publickey],
   keepalive: true
